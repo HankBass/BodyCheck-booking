7289c5759e6aec137a25807d30b087c9093930ba
unknown <youhank_bass@sina.com>
2021-12-14 17:36:48 +0800

    add myPage detailPage listPage

---
+++ pages/list/index.js
@@ -0,0 +1,74 @@
+// pages/list/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    orderList:[
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000",bookingData:{person:"张三",time:"2021-12-06",address:"广东医科大学"}},
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000"},
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000"},
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000"},
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000"},
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000"},
+      {name:"广东医科大学-男-体检分类套餐A",status:"已预约",price:"1000"},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/my/index.js
@@ -0,0 +1,71 @@
+// pages/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    orderList:[
+      {image:"image1",label:"全部订单",icon:"orders-o",bgColor:'#FCF6F6',color:"#8C8484" },
+      {image:"image2",label:"待付款",icon:"balance-pay",bgColor:'#FCF7F1',color:"#8C8484" },
+      {image:"image3",label:"待体检",icon:"guide-o",bgColor:'#FAF7EB',color:"#8C8484"  },
+      {image:"image4",label:"退款",icon:"balance-o",bgColor:'#FFF7EE',color:"#8C8484"  },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
